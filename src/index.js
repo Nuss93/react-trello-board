8c04fea429c520e2178dc0a0cb5b777176c4dc11
Nuss93 <38286277+Nuss93@users.noreply.github.com>
2020-08-10 13:24:51 +0800

    ADDED BOOTSTRAP

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TaskList from './views/TaskList';
+import React from "react";
+import ReactDOM from "react-dom";
+import TaskList from "./views/TaskList";
 // import '@atlaskit/css-reset';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <TaskList />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
